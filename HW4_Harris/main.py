35fc5689ebca2584a8406b379317386641b6d37e
udyankq <156564654+udyankq@users.noreply.github.com>
2026-02-28 19:37:19 +0500

    Update main.py

--- HW4_Harris/main.py
+++ HW4_Harris/main.py
@@ -1,20 +1,15 @@
 import cv2
 import numpy as np
 
-# Загружаем изображение
 img = cv2.imread('HW4_Harris/assets/my_object.jpg')
 
-# Переводим в серый
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 gray = np.float32(gray)
 
-# Harris detector
 dst = cv2.cornerHarris(gray, 2, 3, 0.04)
 
-# Немного усиливаем углы
 dst = cv2.dilate(dst, None)
 
-# Отмечаем углы красным
 img[dst > 0.01 * dst.max()] = [0, 0, 255]
 
 cv2.imshow("Harris Corners", img)
