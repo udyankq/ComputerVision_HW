5f4c2862a670e09bd2bda53d8a6267d1ded0493c
udyankq <156564654+udyankq@users.noreply.github.com>
2026-02-28 19:37:48 +0500

    Update main.py

--- HW5_ORB/main.py
+++ HW5_ORB/main.py
@@ -1,16 +1,12 @@
 import cv2
 
-# Загружаем изображение
 img = cv2.imread('HW5_ORB/assets/my_object2.jpg')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-# Создаем ORB объект
 orb = cv2.ORB_create()
 
-# Находим ключевые точки
 keypoints = orb.detect(gray, None)
 
-# Рисуем точки
 img_with_kp = cv2.drawKeypoints(img, keypoints, None, color=(0,255,0))
 
 cv2.imshow("ORB Features", img_with_kp)
